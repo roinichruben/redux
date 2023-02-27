d5046257535cf3df0f45d94da59ba403dcd1c24c
Ruben Roinich <roinichruben@gmail.com>
2023-02-27 16:17:40 -0300

    store y reducer configurados

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import App from './App';
+import store from './redux/store';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
